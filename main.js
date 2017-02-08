3f6c5d0071181d04326acc12cc003956ff22d8ee
vaultah <waultah@gmail.com>
2017-02-08 08:20:54 +0000

    A few cool improvements, new version

--- main.js
+++ main.js
@@ -6,42 +6,28 @@ if (!("Notification" in window)) {
     Notification.requestPermission();
 }
 
-function make_box(tags) {
-    var box_cls = 'question-tags-popup', queue_cls = 'box-queue',
-        popups = $('.' + box_cls), new_box;
-
-    new_box = $("<div>", {html: '<div>'+tags.join('</div><div>')+'</div>',
-                          class: box_cls})
-    if (!$('#' + queue_cls).length)
-        $('body').prepend($('<div>', { id: queue_cls }));
-
-    $('#' + queue_cls).prepend(new_box);
-
-    // 5 boxes at most
-    if (popups.length >= 5)
-        popups.last().remove();
-}
-
 var audio = new Audio(chrome.extension.getURL('sound.wav'));
 
 setInterval(function() {
     var nf = $('.new-post-activity');
     // Can be empty
     if (nf.length) {
-        // Play sound immediately
-        audio.play();
         nf.trigger('click');
+        audio.play();
         // Number of new questions
         var count = parseInt(nf.text());
         // `count` questions from the top
         var summaries = $('.question-summary').slice(0, count);
         // Iterate in reverse order
         $(summaries.get().reverse()).each(function(i, v) {
-            var tags = $(v).find('.tags').find('a').get().map(function(tag) {
+            v = $(v);
+            var tags = v.find('.tags').find('a').get().map(function(tag) {
                 return $(tag).text();
             });
-            // Display the box
-            make_box(tags);
+
+            // Highlight the question
+            v.attr('class', 'se-notifications-new-post ' + v.attr('class'));
+
             // Emit desktop notification if we can
             if ("Notification" in window && Notification.permission === 'granted') {
                 var title = $('h3', v);
